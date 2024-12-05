5ec3d783508a0bb01d9a21ebc1dfb91dc4b25f6d
Anthony <61360659+Anthony-J-G@users.noreply.github.com>
2024-12-05 08:15:58 -0500

    Deleted some zig boilerplate + editing API

--- utils/discover.zig
+++ utils/discover.zig
@@ -1,6 +1,18 @@
 const std = @import("std");
+const Compile = std.Build.Step.Compile;
+const Module = std.Build.Module;
 
-pub fn main() !void {
+/// Handy when you have many C/C++ source files and want them all to have the same flags.
+pub fn addCSourceFiles(compile: *Compile, options: Module.AddCSourceFilesOptions) void {
+    compile.root_module.addCSourceFiles(options);
+}
+
+/// Handy when you have many C/C++ source files and want them all to have the same flags.
+pub fn discoverCSourceFiles(compile: *Compile, options: Module.AddCSourceFilesOptions) void {
+    compile.root_module.addCSourceFiles(options);
+}
+
+pub fn allYour() void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
@@ -14,11 +26,4 @@ pub fn main() !void {
     try stdout.print("Run `zig build test` to run the tests.\n", .{});
 
     try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
