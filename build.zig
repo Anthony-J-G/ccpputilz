9e8f7774254d2f571c6f14704e9c8e4e9b7478f4
Anthony <61360659+Anthony-J-G@users.noreply.github.com>
2024-12-15 16:18:07 -0500

    Pointing build script to the correct `ccpputilz.zig`

--- build.zig
+++ build.zig
@@ -1,24 +1,23 @@
 const std = @import("std");
 
-const ccpputilz = @import("ccpputilz.zig");
+const ccpputilz = @import("src/ccpputilz.zig");
 pub usingnamespace ccpputilz;
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("ccpputilz", .{
-        .root_source_file = b.path("ccpputilz.zig"),
+    _ = b.addModule("utilz", .{
+        .root_source_file = b.path("src/ccpputilz.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     const tests = b.addTest(.{
-        .root_source_file = b.path("ccpputilz.zig"),
+        .root_source_file = b.path("src/ccpputilz.zig"),
         .target = target,
         .optimize = optimize,
     });
-
     const run_tests = b.addRunArtifact(tests);
 
     const test_step = b.step("test", "Run unit tests");
