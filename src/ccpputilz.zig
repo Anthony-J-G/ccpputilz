34e45ebbb97db4f8a7f5ec387e298b0adaccf207
Anthony <61360659+Anthony-J-G@users.noreply.github.com>
2025-02-24 23:22:35 -0500

    Fixing module imports

--- src/ccpputilz.zig
+++ src/ccpputilz.zig
@@ -1,15 +1,22 @@
-const std = @import("std");
-const testing = std.testing;
+const testing = @import("std").testing;
 
-pub const discover = @import("discover.zig");
-pub const corrosion = @import("corrosion.zig");
-pub const version = @import("version.zig");
-pub const cdb = @import("compile_commands.zig");
-// pub const find_vulkan = @import("find_vulkan.zig");
-pub const gtest = @import("gtest.zig");
+
+pub const discoverCSourceFiles = discover.discoverCSourceFiles;
+
+
+
+pub const discover = @import("modules/discover.zig");
+pub const corrosion = @import("modules/corrosion.zig");
+pub const version = @import("modules/version.zig");
+pub const compiledb = @import("modules/compile_commands.zig");
+// pub const find_vulkan = @import("modules/find_vulkan.zig");
+// pub const autodoc = @import("modules/autodoc.zig")
+pub const gtest = @import("modules/gtest.zig");
 
 /// For internal use only
 const tests = @import("internal/testing.zig");
+const root = @import("root");
+
 
 
 test {
