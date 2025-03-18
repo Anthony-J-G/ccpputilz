1de88f7b27ef3ba2c46c28368c8bc1a3bbe12914
Anthony <61360659+Anthony-J-G@users.noreply.github.com>
2025-03-18 14:50:49 -0400

    [tests] migrating example code to a different sample problem

--- examples/src/lib/samples.cpp
+++ examples/src/lib/samples.cpp
@@ -1,11 +1,7 @@
 #include "samples.hpp"
 #include <math.h>
 
-namespace learning {
-
-double HoeffdingInequality(int N, double epsilon) {
-    return 2 * pow(2.75, -2 * pow(epsilon, 2) * N);
-}
+namespace nnfs {
 
 
 
--- examples/src/lib/samples.hpp
+++ examples/src/lib/samples.hpp
@@ -1,17 +1,12 @@
 #pragma once
 
 
-namespace learning {
+namespace nnfs {
 
-/*
-    Given some sample size N and tolerance epsilon, return the probability that the difference
-    between the underlying sample probability and population probability is greater than epsilon.
-
-    The inequality implies that as the sample size N increases, it becomes exponentially unlikely that the sample probability will deviate from the population probability by more than our
-    tolerance epsilon.
-*/
-double HoeffdingInequality(int N, double epsilon);
+struct Neuron {
+    float* inputs;
+    float bias;
+};
 
 
-
-} // End sample
+} // End nnfs
