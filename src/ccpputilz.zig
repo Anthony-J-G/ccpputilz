36c9ce8f054ec38c193e051eb4e9869dca2615fd
anthony-j-g <anthonyjbasai@gmail.com>
2025-06-20 16:59:34 -0400

    [repo] pulling out modules using different categorizations than previously

--- src/ccpputilz.zig
+++ src/ccpputilz.zig
@@ -1,12 +1,4 @@
-// Default Modules (available by default)
-pub const discover          = @import("default/discover.zig");
-pub const version           = @import("default/version.zig");
-pub const compile_commands  = @import("default/compile_commands.zig");
-pub const tests             = @import("default/tests.zig");
-pub const autodoc           = @import("default/autodoc.zig");
-
-
-// Third Party Extension Modules (require additional dependencies)
+pub const default           = @import("default.zig"); // Default Modules (available by default)
 pub const corrosion         = @import("extensions/corrosion.zig"); // TODO(anthony-j-g): Make it so that if a client tries to call this, they get a @panic error
 pub const gtest             = @import("extensions/gtest.zig");
 pub const find_vulkan       = @import("extensions/find_vulkan.zig");
---
+++ src/default.zig
@@ -0,0 +1,9 @@
+//! Idk, Add some random stuff here
+//! 
+
+
+pub const discover          = @import("default/discover.zig");
+pub const version           = @import("default/version.zig");
+pub const compile_commands  = @import("default/compile_commands.zig");
+pub const tests             = @import("default/tests.zig");
+pub const autodoc           = @import("default/autodoc.zig");
