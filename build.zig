5335f95470f6203e9519767d9a8a3d07e0e1c3fb
anthony-j-g <anthonyjbasai@gmail.com>
2026-02-05 00:37:58 -0500

    [repo] updating to Zig 0.15.2

--- build.zig
+++ build.zig
@@ -1,40 +1,14 @@
 const std = @import("std");
-const ccpputilz = @import("src/ccpputilz.zig");
-pub usingnamespace ccpputilz;
+
+pub const compiledb = @import("src/default/compiledb.zig");
+pub const discover  = @import("src/default/discover.zig");
+
+// pub const extensions = @import("extensions.zig");
 
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+    _ = b;
+    @panic("This dev dependency isn't meant to be build");
 
-    const module = b.addStaticLibrary(.{
-        .name = "utilz",
-        .root_source_file = b.path("src/ccpputilz.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    // Generate Docs
-    const install_docs = b.addInstallDirectory(.{
-        .source_dir = module.getEmittedDocs(),
-        .install_dir = .prefix,
-        .install_subdir = "docs",
-    });
-    const docs_step = b.step("docs", "Install docs into zig-out/docs");
-    docs_step.dependOn(&install_docs.step);
-
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/ccpputilz.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    const unit_tests_cmd = b.addRunArtifact(unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");    
-    test_step.dependOn(&unit_tests_cmd.step);
+    // NOTE(anthony-j-g): Can maybe add a step back to build docs and run tests?
 }
